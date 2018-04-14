f458803ae26f1515b694c74cee233ab2fbf20575
liwwtj <37742545+liwwtj@users.noreply.github.com>
2018-04-14 19:24:19 +0800

    TestAddLocalProject

---
+++ TIJ15/src/generics/Holder3.java
@@ -0,0 +1,17 @@
+package generics;
+
+public class Holder3<T> {
+	private T a;
+	public Holder3(T a){
+		this.a = a;
+	}
+	public T get(){
+		return a;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Holder3<Automobile> h3= new Holder3<>(new Automobile());
+		Automobile a = h3.get();
+	}
+
+}
